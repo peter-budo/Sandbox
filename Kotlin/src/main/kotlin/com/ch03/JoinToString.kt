b0815aafaa1d892d9812af31baec2b2c52cfc783
Peter Miklosko <peter.miklosko@tuitravel.com>
2019-01-16 09:47:54 +0100

    Updated Kotlin version. Chap 3 joinToString example

---
+++ Kotlin/src/main/kotlin/com/ch03/JoinToString.kt
@@ -0,0 +1,17 @@
+package com.ch03
+
+fun main(args: Array<String>) {
+    val list = listOf(1, 2, 3)
+    println(joinToString(list, "; ", "(", ")"))
+}
+
+fun <T> joinToString(collection: Collection<T>, separator: String, prefix: String, postfix: String): String {
+    val result = StringBuilder(prefix)
+    for ((index, element) in collection.withIndex()) {
+        if (index > 0) result.append(separator)
+        result.append(element)
+    }
+
+    result.append(postfix)
+    return result.toString()
+}
